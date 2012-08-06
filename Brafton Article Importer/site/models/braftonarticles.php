502b6a2ed9995298792684013c69e63b37245cbd
Mike Walton <michael.walton@contentlead.com>
2012-08-06 13:55:40 -0400

    rm error reporting

--- Brafton Article Importer/site/models/braftonarticles.php
+++ Brafton Article Importer/site/models/braftonarticles.php
@@ -1,8 +1,6 @@
 <?php
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
-error_reporting(E_ALL );
-ini_set("display_errors", 1);
 jimport('joomla.application.menu');
 jimport('joomla.database.table.category');
 
