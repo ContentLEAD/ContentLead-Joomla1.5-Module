8e7f55e55525ded3868f125deb0a7e2c89da27d1
Mike Walton <michael.walton@contentlead.com>
2012-08-17 14:20:59 -0400

    fixd a sql oopsie

--- Brafton Article Importer/admin/install.sql
+++ Brafton Article Importer/admin/install.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS `#__brafton_options`;
 CREATE TABLE `#__brafton` (
 	`brafton_id` int(25) NOT NULL,
 	`id` int(11) NOT NULL,
-	`pic_id` int(11) NOT NULL,
 	PRIMARY KEY (`brafton_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -21,6 +20,12 @@ CREATE TABLE `#__brafton_options` (
 	`options_name` VARCHAR(50) NULL,  
 	`options_value` VARCHAR(100) NULL,  
 	PRIMARY KEY (`id`) 
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `#__brafton_pics` (
+	`id` int(11) NOT NULL,
+	`brafton_id` int(25) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
  
 INSERT INTO `#__brafton_options` (`id`, `options_name`, `options_value`) VALUES (0, 'braf_api_key', 'Enter API Key');
